3f305aee1721f4df7583909ff28acf59c4771d09
Parikshit127 <parikshitkaushal0712@gmail.com>
2025-11-07 00:14:20 +0530

    Initial commit for server

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const MONGO = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/school-db';
 
 mongoose.connect(MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
